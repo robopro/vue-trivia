bd7179aaca3d7d5d52a144f97144406328e219d9
Morten Salvesen <msalvefirm@gmail.com>
2020-02-28 15:38:13 +0100

    Vue Styleguidist used for documentation. Small refactoring.

--- src/components/LoadingIcon.vue
+++ src/components/LoadingIcon.vue
@@ -10,5 +10,6 @@ export default {
 }
 </script>
 
-<style scoped>
-</style>
+<docs>
+Simple component showing a loading gif.
+</docs>
--- src/views/MainMenu.vue
+++ src/views/MainMenu.vue
@@ -2,21 +2,26 @@
 <div>
   <b-card-header class="custom-info text-white font-weight-bold">New Game</b-card-header>
   <b-card-body class="h-100">
-    <GameForm @form-submitted="onFormSubmit"></GameForm>
+    <GameForm @form-submitted="handleFormSubmitted"></GameForm>
   </b-card-body>
 </div>
 </template>
 
 <script>
 import GameForm from '../components/GameForm'
+
 export default {
   name: 'MainMenu',
   components: {
     GameForm
   },
   methods: {
-    onFormSubmit(data) {
-      const query = data
+    /** Triggered by custom 'form-submitted' event from GameForm child component. 
+     * Parses formData, and route pushes to 'quiz' with formData as query
+     * @public
+     */
+    handleFormSubmitted(formData) {
+      const query = formData
       query.difficulty = query.difficulty.toLowerCase()
       this.$router.push({ name: 'quiz', query: query })
     }
@@ -24,5 +29,6 @@ export default {
 }
 </script>
 
-<style scoped>
-</style>
+<docs>
+Root view. Contains GameForm component - for user to set game options. Routes to GameController view on form submit.
+</docs>
